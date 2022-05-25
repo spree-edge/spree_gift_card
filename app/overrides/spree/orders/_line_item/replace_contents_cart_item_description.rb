5a9477371110ed94d0bed749390c884606d5624e
bluebash-dev <developer@bluebash.co>
2022-05-25 14:22:31 +0530

    minor text change

--- app/overrides/spree/orders/_line_item/replace_contents_cart_item_description.rb
+++ app/overrides/spree/orders/_line_item/replace_contents_cart_item_description.rb
@@ -6,7 +6,7 @@ Deface::Override.new(
   insert_bottom: '[data-hook="cart_item_description"]',
   text: <<-HTML
             <% if line_item.gift_card %>
-              <br/><b><%= Spree.t(:for) %>:</b> <%= line_item.gift_card.name %> (<%= line_item.gift_card.email %>)
+              <br/><b><%= Spree.t(:to) %>:</b> <%= line_item.gift_card.name %> (<%= line_item.gift_card.email %>)
               <br/><b><%= Spree.t(:note) %>:</b> <%= line_item.gift_card.note %>
             <% end %>
   HTML
